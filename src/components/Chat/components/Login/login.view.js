9c3a6e42b3a2fb5fdb0f258e226bfa5843bc318b
Gustavo Morais <gusxmorais@gmail.com>
2020-01-07 19:37:10 -0300

    build: login component terminated and working correctly

--- src/components/Chat/components/Login/login.view.js
+++ src/components/Chat/components/Login/login.view.js
@@ -1,25 +1,40 @@
 import React from 'react';
 import style from './login.module.scss';
+import labels from 'components/scss_modules/labels.module.scss';
 
 const View = props => {
 
   return (
     <div className={style.container}>
-      <p className={style.message}>{ props.message }</p>
-      <form onSubmit={props.handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          onChange={props.handleChanges}
-          value={props.changes.username}
-        />
-        <input
-          type="password"
-          name="password"
-          onChange={props.handleChanges}
-          value={props.changes.password}
-        />
-        <button>Login</button>
+      <p 
+        className={labels.warning} 
+        style={ { display: props.message ? 'block' : 'none' } }>
+        { props.message }
+      </p>
+      <form className={style.form} onSubmit={props.handleSubmit}>
+        <label>
+          <p className={style.inputName}>Username</p>
+          <input
+            className={style.input}
+            type="text"
+            name="username"
+            onChange={props.handleChanges}
+            value={props.changes.username}
+          />
+        </label>
+        <label>
+          <p className={style.inputName}>Password</p>
+          <input
+            className={style.input}
+            type="password"
+            name="password"
+            onChange={props.handleChanges}
+            value={props.changes.password}
+          />
+        </label>
+        <p>
+          <button className={labels.buttonDone}>Login</button>
+        </p>
       </form>
     </div>
   );
