dcbb57c79432b8f33b70e60b61247399690480d2
Gustavo Morais <gusxmorais@gmail.com>
2020-01-14 13:20:58 -0300

    build: adding styled-components to requeriments

--- src/components/Chat/chat.view.js
+++ src/components/Chat/chat.view.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import style from './chat.module.scss';
-import RoomList from './components/RoomList';
-import RoomWindow from './components/RoomWindow';
 import Logoff from './components/Logoff';
 import NewRoom from './components/NewRoom';
-import EnterRoom from './components/EnterRoom';
+import RoomList from './components/RoomList';
+import RoomWindow from './components/RoomWindow';
 
 const View = props => (
   <div className={style.container}>
     <div className={style.blockLeft}>
       <Logoff />
-      <EnterRoom />
       <NewRoom />
       <div className={style.roomList}>
         <RoomList />
